568532125b9697f796b19dba33318f6d236b95bd
mxrmxs <mxrmxs@gmail.com>
2024-09-24 19:53:51 +0200

    Update edit.py

--- Project_01_Band_Name_Generator/edit.py
+++ Project_01_Band_Name_Generator/edit.py
@@ -1,1 +1,4 @@
-# EDIT YOUR SOLUTION HERE
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
